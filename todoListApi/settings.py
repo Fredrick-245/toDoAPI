05240153338a3adde37714cee6101e1355bdfdb5
Fredrick Ndemo <tengeya@Fredricks-MacBook-Pro.local>
2024-07-15 11:39:13 +0300

    Added aunthetication,Modified User Model

--- todoListApi/settings.py
+++ todoListApi/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "aunthetication.User"
+
 
 # Application definition
 
@@ -37,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'aunthetication',
+    'todos',
+    'helpers'
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
